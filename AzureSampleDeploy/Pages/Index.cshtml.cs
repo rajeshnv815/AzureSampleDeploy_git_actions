95a23acb64431819e3c2f0dd9c1febd033ac0186
rajeshnv815 <c0858815@mylambton.ca>
2024-03-08 20:55:56 -0500

    Added Configuration greetings message

--- AzureSampleDeploy/Pages/Index.cshtml.cs
+++ AzureSampleDeploy/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace AzureSampleDeploy.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly IConfiguration _configuration;
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            string? greetings = _configuration.GetSection("Greetings").Value;
+            ViewData["Greetings"]= greetings;
         }
     }
 }
--- AzureSampleDeploy/Pages/Privacy.cshtml.cs
+++ AzureSampleDeploy/Pages/Privacy.cshtml.cs
@@ -14,6 +14,7 @@ namespace AzureSampleDeploy.Pages
 
         public void OnGet()
         {
+            throw new Exception("Testing Insights");
         }
     }
 }
